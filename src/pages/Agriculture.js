69c93cd1ca37ed75077c95aac4747484fd48eaac
Mayuresh Joshi <jmayuresh25@gmail.com>
2023-03-27 10:50:11 +0530

    setup gh-pages

--- src/pages/Agriculture.js
+++ src/pages/Agriculture.js
@@ -1,33 +1,31 @@
 import React, { useState } from "react";
 import '../App.css';
+import { handleDownload } from "../functions";
 
 
 function Agriculture() {
-  const [setActive] = useState("first");
+  //const [setActive] = useState("first");
+  const Ag1 = 'agriculture/seeds.jpg'
+  const Ag2 = 'agriculture/fertilizer.jpg'
+  const Ag3 = 'agriculture/crop.jpg'
 
   return (
     <>
     <button
   className="activeButton"
-  onClick={() => {
-    setActive("Seeds");
-  }}
+  onClick={() => {handleDownload(Ag1)}}
 >
 Seeds
 </button>
 <button
   className="activeButton"
-  onClick={() => {
-    setActive("Fertilize");
-  }}
+  onClick={() => {handleDownload(Ag2)}}
 >
 Fertilize
 </button>
 <button
   className="activeButton"
-  onClick={() => {
-    setActive("Crops");
-  }}
+  onClick={() => {handleDownload(Ag3)}}
 >
 Crops
 </button>
